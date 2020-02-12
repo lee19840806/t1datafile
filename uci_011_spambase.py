5abc8f4e06867ab108a2dacdc2bbff5533c18ef2
steve <leegao36@163.com>
2020-02-12 14:47:42 +0800

    added uci_011_spambase.py

--- uci_011_spambase.py
+++ uci_011_spambase.py
@@ -9,9 +9,9 @@ import pandas # install pandas by "pip install pandas", or install Anaconda dist
 # Warning: the data processing techniques shown below are just for concept explanation, which are not best-proctices
 
 # data set repository
-# https://archive.ics.uci.edu/ml/datasets/Mushroom
+# https://archive.ics.uci.edu/ml/datasets/Spambase
 
-url_data_train = 'https://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.data'
+url_data_train = 'https://archive.ics.uci.edu/ml/machine-learning-databases/spambase/spambase.data'
 
 def download_file(url):
     components = urllib.parse.urlparse(url)
@@ -52,39 +52,77 @@ def download_file(url):
     return io.BytesIO(total)
 
 # download data from UCI Machine Learning Repository
-data_train = download_file(url_data_train)
+#data_train = download_file(url_data_train)
+data_train = '/home/lee1984/Desktop/spambase.data'
 
-# target_poisonous_or_edible is the original target variable, which will be converted into 0 or 1 later
+# spam_nonspam is the original target variable, which will be converted into 0 or 1 later
 columns = [
-    'target_poisonous_or_edible',
-    'cap-shape',
-    'cap-surface',
-    'cap-color',
-    'bruises',
-    'odor',
-    'gill-attachment',
-    'gill-spacing',
-    'gill-size',
-    'gill-color',
-    'stalk-shape',
-    'stalk-root',
-    'stalk-surface-above-ring',
-    'stalk-surface-below-ring',
-    'stalk-color-above-ring',
-    'stalk-color-below-ring',
-    'veil-type',
-    'veil-color',
-    'ring-number',
-    'ring-type',
-    'spore-print-color',
-    'population',
-    'habitat']
+    'word_freq_make',
+    'word_freq_address',
+    'word_freq_all',
+    'word_freq_3d',
+    'word_freq_our',
+    'word_freq_over',
+    'word_freq_remove',
+    'word_freq_internet',
+    'word_freq_order',
+    'word_freq_mail',
+    'word_freq_receive',
+    'word_freq_will',
+    'word_freq_people',
+    'word_freq_report',
+    'word_freq_addresses',
+    'word_freq_free',
+    'word_freq_business',
+    'word_freq_email',
+    'word_freq_you',
+    'word_freq_credit',
+    'word_freq_your',
+    'word_freq_font',
+    'word_freq_000',
+    'word_freq_money',
+    'word_freq_hp',
+    'word_freq_hpl',
+    'word_freq_george',
+    'word_freq_650',
+    'word_freq_lab',
+    'word_freq_labs',
+    'word_freq_telnet',
+    'word_freq_857',
+    'word_freq_data',
+    'word_freq_415',
+    'word_freq_85',
+    'word_freq_technology',
+    'word_freq_1999',
+    'word_freq_parts',
+    'word_freq_pm',
+    'word_freq_direct',
+    'word_freq_cs',
+    'word_freq_meeting',
+    'word_freq_original',
+    'word_freq_project',
+    'word_freq_re',
+    'word_freq_edu',
+    'word_freq_table',
+    'word_freq_conference',
+    'char_freq_;',
+    'char_freq_(',
+    'char_freq_[',
+    'char_freq_!',
+    'char_freq_$',
+    'char_freq_#',
+    'capital_run_length_average',
+    'capital_run_length_longest',
+    'capital_run_length_total',
+    'spam_nonspam']
 
 # convert flat file into pandas dataframe
 df_train = pandas.read_csv(data_train, header = None, names = columns, index_col = False)
 
-# the target variable, 1 = 'p' (poisonous) and 0 = 'e' (edible)
-df_train['target_poisonous_or_edible'] = df_train['target_poisonous_or_edible'].apply(lambda x: 1 if x == 'p' else 0)
+# the target variable, 1 = spam and 0 = non-spam
+# we insert target_spam_nonspam into the dataframe as the first column, and drop the original spam_nonspam column
+df_train.insert(0, 'target_spam_nonspam', df_train['spam_nonspam'])
+df_train = df_train.drop('spam_nonspam', axis = 1)
 
 # save the dataframe as CSV file, you can zip it, upload it to t1modeler.com, and build a model
-df_train.to_csv('mushroom.csv', index = False)
+df_train.to_csv('spambase.csv', index = False)
