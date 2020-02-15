364b7283554e9206dc65a7364fbea431f609b700
steve <leegao36@163.com>
2020-02-15 23:53:16 +0800

    added uci_031_eeg_eye_state.py

---
+++ uci_031_eeg_eye_state.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import urllib
+import http
+import io
+import pandas # install pandas by "pip install pandas", or install Anaconda distribution (https://www.anaconda.com/)
+
+# Warning: the data processing techniques shown below are just for concept explanation, which are not best-proctices
+
+# data set repository
+# https://archive.ics.uci.edu/ml/datasets/EEG+Eye+State
+
+url_data_train = 'https://archive.ics.uci.edu/ml/machine-learning-databases/00264/EEG%20Eye%20State.arff'
+
+def download_file(url):
+    components = urllib.parse.urlparse(url)
+    conn = http.client.HTTPConnection(components.hostname, port = components.port)
+    conn.request('GET', components.path)
+    resp = conn.getresponse()
+
+    if resp.status != 200:
+        resp.close()
+        conn.close()
+        raise ValueError('Error: {0}'.format(resp.reason))
+
+    print('\rStarted', end = '\r')
+    content_length = resp.getheader('Content-Length')
+    if content_length is None:
+        content_length = '(total: unknown)'
+    else:
+        content_length = int(content_length)
+        if content_length < 1024:
+            content_length_str = '(total %.0f Bytes)' % content_length
+        elif content_length < 1024 * 1024:
+            content_length_str = '(total %.0f KB)' % (content_length / 1024)
+        else:
+            content_length_str = '(total %.1f MB)' % (content_length / 1024 / 1024)
+
+    total = bytes()
+    while not resp.isclosed():
+        total += resp.read(10 * 1024)
+        if len(total) < 1024:
+            print(('\rDownloaded: %.0f Bytes ' % len(total)) + content_length_str + '  ', end = '\r')
+        if len(total) < 1024 * 1024:
+            print(('\rDownloaded: %.0f KB ' % (len(total) / 1024)) + content_length_str + '  ', end = '\r')
+        else:
+            print(('\rDownloaded: %.1f MB ' % (len(total) / 1024 / 1024)) + content_length_str + '  ', end = '\r')
+
+    print()
+    conn.close()
+    return io.BytesIO(total)
+
+# download data from UCI Machine Learning Repository
+data_train = download_file(url_data_train)
+
+# locate data rows by using '@data\n' as a split word
+content_data = io.BytesIO(data_train.read().decode().split('@data')[-1].encode())
+
+# eyeDetection is the original target variable, which will be converted into 0 or 1 later
+columns = [
+    'AF3',
+    'F7',
+    'F3',
+    'FC5',
+    'T7',
+    'P7',
+    'O1',
+    'O2',
+    'P8',
+    'T8',
+    'FC6',
+    'F4',
+    'F8',
+    'AF4',
+    'eyeDetection']
+
+# convert flat files into pandas dataframes
+df_train = pandas.read_csv(content_data, header = None, names = columns, index_col = False)
+
+# the target variable, we insert target_eyeDetection into the dataframe as the first column and drop the original eyeDetection column
+df_train.insert(0, 'target_eyeDetection', df_train['eyeDetection'])
+df_train = df_train.drop('eyeDetection', axis = 1)
+
+# save the dataframe as CSV file, you can zip it, upload it to t1modeler.com, and build a model
+df_train.to_csv('uci_031_eeg_eye_state.csv', index = False)
