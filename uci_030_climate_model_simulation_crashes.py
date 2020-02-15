9c0def9f06672fc81e4e2707fb8cceb74976cb27
steve <leegao36@163.com>
2020-02-15 23:24:49 +0800

    added uci_030_climate_model_simulation_crashes.py

---
+++ uci_030_climate_model_simulation_crashes.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import urllib
+import http
+import io
+import numpy
+import pandas # install pandas by "pip install pandas", or install Anaconda distribution (https://www.anaconda.com/)
+
+# Warning: the data processing techniques shown below are just for concept explanation, which are not best-proctices
+
+# data set repository
+# https://archive.ics.uci.edu/ml/datasets/Climate+Model+Simulation+Crashes
+
+url_data_train = 'https://archive.ics.uci.edu/ml/machine-learning-databases/00252/pop_failures.dat'
+
+def download_file(url):
+    components = urllib.parse.urlparse(url)
+    conn = http.client.HTTPConnection(components.hostname, port = components.port)
+    conn.request('GET', components.path)
+    resp = conn.getresponse()
+
+    if resp.status != 200:
+        resp.close()
+        conn.close()
+        raise ValueError('Error: {0}'.format(resp.reason))
+
+    print('\rStarted', end = '\r')
+    content_length = resp.getheader('Content-Length')
+    if content_length is None:
+        content_length = '(total: unknown)'
+    else:
+        content_length = int(content_length)
+        if content_length < 1024:
+            content_length_str = '(total %.0f Bytes)' % content_length
+        elif content_length < 1024 * 1024:
+            content_length_str = '(total %.0f KB)' % (content_length / 1024)
+        else:
+            content_length_str = '(total %.1f MB)' % (content_length / 1024 / 1024)
+
+    total = bytes()
+    while not resp.isclosed():
+        total += resp.read(10 * 1024)
+        if len(total) < 1024:
+            print(('\rDownloaded: %.0f Bytes ' % len(total)) + content_length_str + '  ', end = '\r')
+        if len(total) < 1024 * 1024:
+            print(('\rDownloaded: %.0f KB ' % (len(total) / 1024)) + content_length_str + '  ', end = '\r')
+        else:
+            print(('\rDownloaded: %.1f MB ' % (len(total) / 1024 / 1024)) + content_length_str + '  ', end = '\r')
+
+    print()
+    conn.close()
+    return io.BytesIO(total)
+
+# download data from UCI Machine Learning Repository
+data_train = download_file(url_data_train)
+
+# convert flat file into pandas dataframe
+df_train = pandas.read_csv(data_train, delimiter = '\s+', header = 0, index_col = False)
+
+# drop columns which are inappropriate for modeling
+df_train = df_train.drop(['Study', 'Run'], axis = 1)
+
+# insert target_outcome into the dataframe as first column, and drop the original outcome column
+df_train.insert(0, 'target_outcome', df_train['outcome'])
+df_train = df_train.drop('outcome', axis = 1)
+
+# save the dataframe as CSV file, you can zip it, upload it to t1modeler.com, and build a model
+df_train.to_csv('uci_030_climate_model_simulation_crashes.csv', index = False)
