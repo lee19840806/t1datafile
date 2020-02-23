6f1c8478a671e9601c3a0b7b1b7d1e57b589c209
steve <leegao36@163.com>
2020-02-23 18:23:18 +0800

    all scripts, import import urllib.request instead of import urllib

--- uci_002_bank_marketing.py
+++ uci_002_bank_marketing.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import urllib
+import urllib.request
 import io
 import zipfile
 import numpy
--- uci_005_cylinder_bands.py
+++ uci_005_cylinder_bands.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import urllib
+import urllib.request
 import io
 import pandas # install pandas by "pip install pandas", or install Anaconda distribution (https://www.anaconda.com/)
 
